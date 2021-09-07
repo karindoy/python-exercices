fa5e6a0979b85f823f4f41bfe33ad9ca0bd81639
karin <doykarin@gmail.com>
2021-09-07 17:30:07 -0300

    Recebe lado de um quadrado e imprime o perímetro e sua área.

---
+++ quadrado.py
@@ -0,0 +1,6 @@
+larguraQuadrado = int(input('Digite o valor correspondente ao lado de um quadrado: '));
+
+perimetro = larguraQuadrado * 4;
+area = larguraQuadrado ** 2;
+
+print('perímetro:', perimetro ,'- área:', area);
