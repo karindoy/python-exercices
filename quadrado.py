07090e8ed2a9ed71cfbc5557d8fe8ec02c90850c
karin <doykarin@gmail.com>
2021-09-07 19:11:34 -0300

    add comment

--- quadrado.py
+++ quadrado.py
@@ -1,3 +1,6 @@
+# programa em Python que receba (entrada de dados) o valor correspondente ao lado de um quadrado, 
+# calcule e imprima (saída de dados) seu perímetro e sua área.
+
 larguraQuadrado = int(input('Digite o valor correspondente ao lado de um quadrado: '));
 
 perimetro = larguraQuadrado * 4;
