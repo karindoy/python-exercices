51f33a2d232d1e3799dccf1456a06614e51f73de
karin <doykarin@gmail.com>
2021-09-07 19:06:09 -0300

    receive 4 grades and calculates the average.

---
+++ medias.py
@@ -0,0 +1,10 @@
+# programa em Python que receba quatro notas, calcule e imprima a média aritmética.
+
+n1 = float(input('Digite a primeira nota: '));
+n2 = float(input('Digite a segunda nota: '));
+n3 = float(input('Digite a terceira nota: '));
+n4 = float(input('Digite a quarta nota: '));
+
+media = (n1 + n2 + n3 + n4)/4;
+
+print('A média aritmética é', media)
