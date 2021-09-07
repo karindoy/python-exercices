a1f16b6f2ee6bff9dbef7ed6a255361d5f5eb70e
karin <doykarin@gmail.com>
2021-09-07 19:12:04 -0300

    changed the messages

--- segundos.py
+++ segundos.py
@@ -1,7 +1,7 @@
 # programa em Python que, dada a quantidade de segundos, "quebre" esse valor em dias, horas, minutos e segundos. 
 # A saída deve estar no formato: a dias, b horas, c minutos e d segundos. 
 
-tempoEmSegundos = int(input("Digite o tempo em segundos: "));
+tempoEmSegundos = int(input("Por favor, entre com o número de segundos que deseja converter: "));
 
 tempoEmMinutos = tempoEmSegundos // 60;
 restoEmSegundos = tempoEmSegundos % 60;
@@ -12,5 +12,5 @@ tempoEmMinutos = tempoEmMinutos % 60;
 tempoEmDias = tempoEmHoras // 24;
 tempoEmHoras = tempoEmHoras % 24;
 
-print(tempoEmDias, 'dias', tempoEmHoras, 'horas' , tempoEmMinutos, ' min',  restoEmSegundos, 'seg .');
+print(tempoEmDias, 'dias,', tempoEmHoras, 'horas,' , tempoEmMinutos, 'minutos e',  restoEmSegundos, 'segundos.');
 
